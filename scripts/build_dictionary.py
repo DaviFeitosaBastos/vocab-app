d92718b29fa05f9b449a532963490b791936696a
Davi Feitosa <davifeitosabastos@gmail.com>
2026-07-01 16:35:59 -0400

    feat: dictionary loader is complete

--- scripts/build_dictionary.py
+++ scripts/build_dictionary.py
@@ -3,29 +3,49 @@ import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-def load_json(relative_path):
+def load_json(relative_path: str):
     absolute_path = os.path.join(script_dir, relative_path)
     with open(absolute_path, "r", encoding="utf-8") as file:
         return json.load(file)
+    
+def save_json(relative_path: str, result: list):
+    absolute_path = os.path.join(script_dir, relative_path)
+    with open(absolute_path, "w", encoding="utf-8") as file:
+        return json.dump(result, file, indent=4)
 
 merged = load_json("../data/raw/formated_words.json")
 wordfreq = load_json("../data/raw/word_frequency.json")
 
-print(f"Merged loaded: {len(merged)} words")
-print(f"Wordfreq loaded: {len(wordfreq)} words")
-
-
-
-def build_dictionary(merged, wordfreq):
+def build_dictionary(merged: list, wordfreq: list) -> list:
+    """ 
+    Receive parameters merged and wordfreq and return the dictionary
+    """
     dictionary = []
     counting = 0 
-
-    for index, value in enumerate(wordfreq):
+    index = 1
+    for value in wordfreq:
         if value[0].upper() in merged:
-            print(index, value[0].upper())
+            data = merged[value[0].upper()]
+
+            if not data["MEANINGS"] == []:
+                grammar_class = data["MEANINGS"][0][0]
+                definitions = data["MEANINGS"][0][1]
+                dictionary.append({
+                    "Id": index,
+                    "Word": value[0].lower(),
+                    "Meanings": [grammar_class, definitions],
+                    "Synonyms": data["SYNONYMS"],
+                })
+                index += 1
         else:
             counting += 1
 
     return dictionary
 
-print(build_dictionary(merged, wordfreq))
+result = build_dictionary(merged, wordfreq)
+save_json("../data/processed/dictionary.json", result)
+
+if __name__ == "__main__":
+    print(f"Merged loaded: {len(merged)} words")
+    print(f"Wordfreq loaded: {len(wordfreq)} words")
+    print(f"Dictionary saved: {len(result)} words")
