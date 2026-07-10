9346d60474fce06d3fe71dee0dd88a0fdbc0d4f6
Davi Feitosa <davifeitosabastos@gmail.com>
2026-07-10 19:00:37 -0400

    feat: added the search menu

--- app/ui.py
+++ app/ui.py
@@ -24,14 +24,19 @@ def main_menu():
     clear()
     print(Panel("[bold blue]Choose one of those sections![/]\n"
     "[bold blue]1[/] - Get a random word\n"
-    "[bold blue]2[/] - Show all progress\n"
-    "[bold blue]3[/] - Add a new word in the dictionary\n"
+    "[bold blue]2[/] - Show all progress\n" \
+    "[bold blue]3[/] - Search a Word\n"
+    "[bold blue]4[/] - Add a new word in the dictionary\n"
     "[red]0 - Exit", title="[cyan]Vocabulary app", subtitle="[cyan]Enjoy", style="cyan"))
 
 def return_option():
     clear()
     print(Panel("[bold blue]Would you like to return? Press [Y]es and [N]ot![/]", title="[cyan]Returning menu", subtitle="[cyan]↓ Prompt ↓", style="cyan"))
     answer = Prompt.ask("[bold blue]Type here")
+
+def search_menu():
+    clear()
+    print(Panel("[bold blue]Type what word would you like to see?[/]", title="[cyan]Searching menu", subtitle="[cyan]↓ Word ↓", style="cyan"))
 
     
 # loading_screen(0.2)
