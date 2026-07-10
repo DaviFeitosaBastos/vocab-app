7701dc9c20cf7083ec27ac08eb97062f069e5562
Davi Feitosa <davifeitosabastos@gmail.com>
2026-07-10 13:32:33 -0400

    feat: log handler

---
+++ app/log_utils.py
@@ -0,0 +1,20 @@
+import sys
+import logging
+from rich.logging import RichHandler
+from pathlib import Path
+
+def get_base_dir():
+    if hasattr(sys, '_MEIPASS'):
+        return Path(sys.executable).parent
+    return Path(__file__).resolve().parents[1]
+
+def get_logger(name: str) -> logging.Logger:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s] %(levelname)s | %(name)s | %(message)s",
+        handlers=[
+            RichHandler(rich_tracebacks=True),
+            logging.FileHandler(get_base_dir() / "app.log")
+        ]
+    )
+    return logging.getLogger(name)
