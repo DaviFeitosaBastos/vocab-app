48c8ef35a1a00f1efc4c0745ef307d2d4f2f9d0f
Davi Feitosa <davifeitosabastos@gmail.com>
2026-07-26 11:24:03 -0400

    refactor: fixed the formating

--- app/log_utils.py
+++ app/log_utils.py
@@ -3,10 +3,12 @@ import logging
 from rich.logging import RichHandler
 from pathlib import Path
 
+
 def get_base_dir():
-    if hasattr(sys, '_MEIPASS'):
+    if hasattr(sys, "_MEIPASS"):
         return Path(sys.executable).parent
     return Path(__file__).resolve().parents[1]
+
 
 def get_logger(name: str) -> logging.Logger:
     logging.basicConfig(
@@ -14,7 +16,7 @@ def get_logger(name: str) -> logging.Logger:
         format="[%(asctime)s] %(levelname)s | %(name)s | %(message)s",
         handlers=[
             RichHandler(rich_tracebacks=True),
-            logging.FileHandler(get_base_dir() / "app.log")
-        ]
+            logging.FileHandler(get_base_dir() / "app.log"),
+        ],
     )
     return logging.getLogger(name)
