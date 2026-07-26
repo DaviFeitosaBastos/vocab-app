11bf35b2ca7868ac282bbc37f9c87d130f7c5e61
Davi Feitosa <davifeitosabastos@gmail.com>
2026-07-26 11:23:08 -0400

    refactor: fixed the formating

--- app/main.py
+++ app/main.py
@@ -1,23 +1,30 @@
 from rich import print
 from rich.prompt import Prompt
 from rich.panel import Panel
-from ui import main_menu, loading_screen, clear, return_option, search_menu
-from utils import search_word, load_dictionary, load_progress, get_daily_word, save_progress, get_unseen, mark_as_seen
+from ui import main_menu, clear, search_menu
+from utils import (
+    search_word,
+    load_dictionary,
+    load_progress,
+    get_daily_word,
+    save_progress,
+    get_unseen,
+    mark_as_seen,
+)
 from log_utils import get_logger
 from time import sleep
 from ui import build_progress_table
 
 log = get_logger(__name__)
 
+
 def main():
     load_files = load_progress("progress.json")
     load_dict = load_dictionary("../data/processed/dictionary.json")
 
-    # loading_screen(0.1)
-
     while True:
         main_menu()
-        try:    
+        try:
             choose = int(Prompt.ask("[bold blue]Choose here"))
             match choose:
                 case 1:
@@ -25,12 +32,19 @@ def main():
                     daily_word = get_daily_word(unseen)
                     save_progress(mark_as_seen(daily_word), load_files, "progress.json")
                     clear()
-                    print(Panel(f"[bold blue]The word today is[/]\n"f"[bold cyan]★ [bold blue]{daily_word[0]['Word']}[/]\n★ [bold blue]Meaning[/] 🢚 {daily_word[0]['Meanings']}[/]", title="[cyan]Daily word", style="cyan",))
+                    print(
+                        Panel(
+                            f"[bold blue]The word today is[/]\n"
+                            f"[bold cyan]★ [bold blue]{daily_word[0]['Word']}[/]\n★ [bold blue]Meaning[/] 🢚 {daily_word[0]['Meanings']}[/]",
+                            title="[cyan]Daily word",
+                            style="cyan",
+                        )
+                    )
                     Prompt.ask("[bold blue]Press enter to return")
 
                 case 2:
                     clear()
-                    sequence = {(x + 1): y for x, y in enumerate(load_files['Seen'])}
+                    sequence = {(x + 1): y for x, y in enumerate(load_files["Seen"])}
                     table = build_progress_table(sequence, load_dict)
                     print(Panel(table, title="[cyan]★ Progress ★", style="cyan"))
                     Prompt.ask("[bold blue]Press enter to return")
@@ -40,19 +54,28 @@ def main():
                     result = search_word(load_dict, word)
                     if result:
                         clear()
-                        print(Panel(
-                            f"[bold cyan]Id: {result['Id']}[/]\n"
-                            f"[bold cyan]Word: {result['Word'].capitalize()}[/]\n"
-                            f"[bold cyan]Meanings: [/][italic]{result['Meanings'][0]}[/] — {result['Meanings'][1]}\n\n"
-                            f"[dim]Synonyms: {', '.join(result['Synonyms'])}[/]",
-                            title="[cyan]Word", style="cyan"
-                        ))
+                        print(
+                            Panel(
+                                f"[bold cyan]Id: {result['Id']}[/]\n"
+                                f"[bold cyan]Word: {result['Word'].capitalize()}[/]\n"
+                                f"[bold cyan]Meanings: [/][italic]{result['Meanings'][0]}[/] — {result['Meanings'][1]}\n\n"
+                                f"[dim]Synonyms: {', '.join(result['Synonyms'])}[/]",
+                                title="[cyan]Word",
+                                style="cyan",
+                            )
+                        )
                         Prompt.ask("[bold blue]Press enter to return to menu")
                     else:
                         clear()
-                        print(Panel(f"[cyan]The word [red]|{word}|[/] was not found[/]", title="[red]Word not found!",style="red"))
+                        print(
+                            Panel(
+                                f"[cyan]The word [red]|{word}|[/] was not found[/]",
+                                title="[red]Word not found!",
+                                style="red",
+                            )
+                        )
                         Prompt.ask("[bold blue]Press enter to return to menu")
-                        
+
                 case 0:
                     print(f"[red]Exitting...")
                     sleep(0.6)
@@ -70,6 +93,6 @@ def main():
         except KeyboardInterrupt:
             print(f"[bold blue]Ok bye!!")
 
+
 if __name__ == "__main__":
     main()
-    
