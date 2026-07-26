da0741f2614578cd6f5b9a585afad6db06862ee6
Davi Feitosa <davifeitosabastos@gmail.com>
2026-07-26 11:23:53 -0400

    refactor: fixed the formating and removed imports unnussed

--- app/ui.py
+++ app/ui.py
@@ -1,49 +1,62 @@
 from rich import print
 import os
-from time import sleep
-from rich.progress import track
 from rich.panel import Panel
 import platform
 from rich.prompt import Prompt
 from rich.table import Table
 
+
 def clear():
     os.system("clear") if not platform.system() == "windows" else "cls"
 
-def loading_screen(s: float):
+
+def main_menu():
     clear()
-    for i in track(range(15), description="[blue]Downloading dictionary..."):
-        sleep(s)  # Simulate work being done
-    clear()
-    for i in track(range(9), description="[green]Setting up..."):
-        sleep(s)
-    clear()
-    for i in track(range(3), description="[green]Loading app..."):
-        sleep(s)
-    
-def main_menu(): 
-    clear()
-    print(Panel("[bold blue]Choose one of those sections![/]\n"
-    "[bold blue]1[/] - Get a random word\n"
-    "[bold blue]2[/] - Show all progress\n" \
-    "[bold blue]3[/] - Search a Word\n"
-    "[red]0 - Exit", title="[cyan]Vocabulary app", subtitle="[cyan]Enjoy", style="cyan"))
+    print(
+        Panel(
+            "[bold blue]Choose one of those sections![/]\n"
+            "[bold blue]1[/] - Get a random word\n"
+            "[bold blue]2[/] - Show all progress\n"
+            "[bold blue]3[/] - Search a Word\n"
+            "[red]0 - Exit",
+            title="[cyan]Vocabulary app",
+            subtitle="[cyan]Enjoy",
+            style="cyan",
+        )
+    )
+
 
 def return_option():
     clear()
-    print(Panel("[bold blue]Would you like to return? Press [Y]es and [N]ot![/]", title="[cyan]Returning menu", subtitle="[cyan]↓ Prompt ↓", style="cyan"))
+    print(
+        Panel(
+            "[bold blue]Would you like to return? Press [Y]es and [N]ot![/]",
+            title="[cyan]Returning menu",
+            subtitle="[cyan]↓ Prompt ↓",
+            style="cyan",
+        )
+    )
     answer = Prompt.ask("[bold blue]Type here")
+
 
 def search_menu():
     clear()
-    print(Panel("[bold blue]Type what word would you like to see?[/]", title="[cyan]Searching menu", subtitle="[cyan]↓ Word ↓", style="cyan"))
+    print(
+        Panel(
+            "[bold blue]Type what word would you like to see?[/]",
+            title="[cyan]Searching menu",
+            subtitle="[cyan]↓ Word ↓",
+            style="cyan",
+        )
+    )
+
 
 def build_progress_table(sequence: dict, dict_: dict) -> Table:
     table = Table(show_header=False, box=None, padding=(0, 1))
-    for k, v in sequence.items(): 
+    for k, v in sequence.items():
         table.add_row(f"[bold cyan]{k}.[/]", str(v))
     return table
-    
+
+
 # loading_screen(0.2)
 # main_menu()
-
